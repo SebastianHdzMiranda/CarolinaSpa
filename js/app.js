b5418a9fa9103d5156a276a4e9885c0d791a37f1
SebastianHdzMiranda <sebashm68@gmail.com>
2023-07-21 16:42:12 -0600

    modificando seccion de servicios

--- js/app.js
+++ js/app.js
@@ -16,19 +16,19 @@
 //     navigation: true, //flechas a los costados para cambiar de img
 // });
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
-    // autoplay:{
-    //     delay: 3000,
-    // },
-});
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     loop: true,
+//     // autoplay:{
+//     //     delay: 3000,
+//     // },
+// });
